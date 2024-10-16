9407ce083030fbccb983c32678366212102cd868
Irvan Yoga Pratama_0770 <117559051+HazeRazer21@users.noreply.github.com>
2024-10-16 16:33:41 +0700

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,24 @@
+/*Menu lateral - mobile*/
+
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+
+menuMobile.addEventListener("click", () =>{
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")/* If ternario  */
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
+});
+
+// Fecha o menu quando clicar em algum item e muda o iconi para list
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
